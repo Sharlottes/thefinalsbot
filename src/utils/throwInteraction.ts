0b322215f45ac9cbdfcdfc9670b7c1e60fbb6730
Sharlotte <aaa9810321@gmail.com>
2024-04-18 15:38:19 +0900

    prov: ignore thrown interaction defer err

--- src/utils/throwInteraction.ts
+++ src/utils/throwInteraction.ts
@@ -1,6 +1,6 @@
 export default async function throwInteraction(
   interaction: Discord.RepliableInteraction,
 ) {
-  await interaction.deferReply({ ephemeral: true });
+  await interaction.deferReply({ ephemeral: true }).catch(() => {});
   await interaction.deleteReply().catch(() => {});
 }
