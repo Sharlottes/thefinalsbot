74a7e4de1d350796c4fc8eeefc388fec533ca130
Sharlotte <aaa9810321@gmail.com>
2024-04-18 15:45:12 +0900

    ah

--- src/express.ts
+++ src/express.ts
@@ -1,3 +1,4 @@
+import { createServer } from "node:http";
 import express from "express";
 const app = express();
 
@@ -5,4 +6,9 @@ app.get("/", (_, res) => {
   res.status(200).setHeader("Content-Type", "text/plain").send("OK");
 });
 
+const server = createServer(express);
+
+server.listen(8000, () => {
+  console.log("server running at http://localhost:8000");
+});
 export default app;
