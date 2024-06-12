f11139eabc05bf16d1cceb6578a4b43117bc041f
Sharlotte <aaa9810321@gmail.com>
2024-06-12 17:19:00 +0900

    feat: 특정 서버의 초대 메시지 삭제

---
+++ src/discord/features/InviteLinkRemover.ts
@@ -0,0 +1,19 @@
+import Vars from "@/Vars";
+import { Discord, On } from "discordx";
+
+@Discord()
+export default class InviteLinkRemover {
+  @On({ event: "messageCreate" })
+  private async messageCreate(
+    [message]: DiscordX.ArgsOf<"messageCreate">,
+    client: DiscordX.Client,
+  ) {
+    const matchArr = message.content.match(/discord.gg\/([A-Za-z0-9]+)/);
+    if (!matchArr) return;
+    const inviteCode = matchArr[1];
+    const result = (await client.rest.get(`/invites/${inviteCode}`, {})) as any;
+    if (Vars.banInviteGuilds.includes(result.guild.id)) {
+      message.delete();
+    }
+  }
+}
