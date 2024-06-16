9d9192e3795541648b6228395be8567f2bb932fe
sharlotte <aaa9810321@gmail.com>
2024-06-16 14:00:41 +0900

    fix: 방생성 임베드 이름을 환경변수에 명시

--- src/Vars.ts
+++ src/Vars.ts
@@ -4,7 +4,7 @@ export default class Vars {
   static client: DiscordX.Client;
   static mainGuild: Discord.Guild;
   static masterUsers: Discord.User[] = [];
-  static roomMakingChannels: Discord.TextChannel[] = [];
+  static roomMakingAnnounceChannels: Record<string, Discord.TextChannel> = {};
   static dmLogChannel: Discord.TextChannel;
   static matchMakingAnnounceChannel: Discord.TextChannel;
   static matchMakingWaitingChannel: Discord.VoiceBasedChannel;
@@ -16,15 +16,26 @@ export default class Vars {
       client.guilds
         .fetch(process.env.TEST_GUILD_ID)
         .then((g) => (Vars.mainGuild = g)),
-      ...process.env.ROOMMAKING_ANNOUNCE_CHANNELS_ID.split(",").map((id) =>
-        client.channels
-          .fetch(id)
-          .then((c) =>
-            Vars.roomMakingChannels.push(
-              Vars.validateChannel(c, ChannelType.GuildText),
-            ),
-          ),
-      ),
+      new Promise(async (res, rej) => {
+        const arr = process.env.ROOMMAKING_ANNOUNCE_CHANNELS_ID.split(",");
+        const promises = [];
+        for (let i = 0; i < arr.length; i += 2) {
+          const name = arr[i],
+            id = arr[i + 1];
+          promises.push(
+            client.channels
+              .fetch(id)
+              .then(
+                (c) =>
+                  (Vars.roomMakingAnnounceChannels[name] = Vars.validateChannel(
+                    c,
+                    ChannelType.GuildText,
+                  )),
+              ),
+          );
+        }
+        await Promise.all(promises).then(res).catch(rej);
+      }),
       ...process.env.MASTER_USERS.split(",").map((id) =>
         client.users.fetch(id).then((u) => Vars.masterUsers.push(u)),
       ),
