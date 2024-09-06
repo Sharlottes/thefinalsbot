43671cea9859ad7a28b84f64670ae4373bebfb89
Sharlotte <aaa9810321@gmail.com>
2024-09-06 13:22:33 +0900

    feat: 방 생성자 설정 service/handler 분리, CRUD 전체 구현

---
+++ src/discord/commands/ServerSettingHandler.ts
@@ -0,0 +1,143 @@
+import ServerSettingManager from "@/core/ServerSettingManager";
+import ServerSettingModel from "@/models/ServerSetting";
+import autoDeleteMessage from "@/utils/autoDeleteMessage";
+import { PermissionGuard } from "@discordx/utilities";
+import {
+  Discord,
+  Guard,
+  Slash,
+  SlashChoice,
+  SlashGroup,
+  SlashOption,
+} from "discordx";
+import MasterGuard from "../guards/MasterGuard";
+import {
+  ApplicationCommandOptionType,
+  channelMention,
+  ChannelType,
+  EmbedBuilder,
+} from "discord.js";
+import ErrorMessageManager from "../messageManagers/ErrorMessageManager";
+import Vars from "@/Vars";
+import PrimitiveInputMessageManager from "../messageManagers/inputs/PrimitiveInputMessageManager";
+import {
+  InputResolvers,
+  TextInputResolver,
+} from "../messageManagers/inputs/InputResolvers";
+import RoomMakingDataModel from "@/models/RoomMakingDataModel";
+import RoomMaker from "../features/RoomsMaker";
+import ServerSettingService from "./ServerSettingService";
+import SlashOptionBuilder from "@/utils/SlashOptionBuilder";
+
+const roomMakerChannelOption = SlashOptionBuilder.create({
+  name: "채널",
+  description: "방 생성자가 등록되거나 등록될 채널",
+  type: ApplicationCommandOptionType.Channel,
+});
+const roomMakerNameOption = SlashOptionBuilder.create({
+  name: "이름",
+  description: "방 생성자의 이름 (ex: 월투, 터랭)",
+  type: ApplicationCommandOptionType.String,
+});
+const roomMakerDescriptionOption = SlashOptionBuilder.create({
+  name: "설명",
+  description: "방 생성자의 설명",
+  type: ApplicationCommandOptionType.String,
+});
+
+@Discord()
+@SlashGroup({
+  name: "서버설정",
+  description: "이 서버에 대한 여러 설정을 관리합니다.",
+})
+@SlashGroup({
+  name: "방생성",
+  description: "해당 채널에 대한 파티방 생성 정보를 조회/수정합니다.",
+  root: "서버설정",
+})
+@Guard(PermissionGuard(["Administrator"]), MasterGuard)
+export default class ServerSettingHandler {
+  @SlashGroup("서버설정")
+  @Slash({ name: "초기화", description: "서버 채널 설정을 새로 정합니다." })
+  async setting(interaction: Discord.ChatInputCommandInteraction) {
+    interaction.deferReply({ ephemeral: true });
+    const serverSettings = await ServerSettingModel.findOne({
+      guildId: interaction.guildId,
+      botId: interaction.client.user!.id,
+    });
+    if (!serverSettings) {
+      throw new Error("서버 설정이 존재치 않는건 불가능합니다.");
+    }
+    autoDeleteMessage(interaction.reply("채널 설정을 변경합니다."), 1500);
+    await ServerSettingManager.main.requestSettingInit(interaction.guild!);
+  }
+
+  @SlashGroup("방생성", "서버설정")
+  @Slash({
+    name: "추가",
+    description: "방 생성자를 새로 등록합니다.",
+  })
+  async createNewRoomMaker(
+    @SlashOption(roomMakerChannelOption)
+    channel: Discord.TextChannel | null,
+    @SlashOption(roomMakerNameOption)
+    name: string | null,
+    @SlashOption(roomMakerDescriptionOption)
+    description: string | null,
+    interaction: Discord.ChatInputCommandInteraction,
+  ) {
+    ServerSettingService.main.addRoomMaker(
+      interaction,
+      channel ?? undefined,
+      name ?? undefined,
+      description ?? undefined,
+    );
+  }
+
+  @SlashGroup("방생성", "서버설정")
+  @Slash({
+    name: "수정",
+    description: "기존 방 생성자를 수정합니다.",
+  })
+  async editRoomMaker(
+    @SlashOption(roomMakerChannelOption)
+    channel: Discord.TextChannel | null,
+    @SlashOption(roomMakerNameOption)
+    name: string | null,
+    @SlashOption(roomMakerDescriptionOption)
+    description: string | null,
+    interaction: Discord.ChatInputCommandInteraction,
+  ) {
+    ServerSettingService.main.editRoomMaker(
+      interaction,
+      channel ?? undefined,
+      name ?? undefined,
+      description ?? undefined,
+    );
+  }
+
+  @SlashGroup("방생성", "서버설정")
+  @Slash({
+    name: "삭제",
+    description: "기존 방 생성자를 삭제합니다.",
+  })
+  async removeRoomMaker(
+    @SlashOption(roomMakerChannelOption)
+    channel: Discord.TextChannel | null,
+    interaction: Discord.ChatInputCommandInteraction,
+  ) {
+    ServerSettingService.main.removeRoomMaker(
+      interaction,
+      channel ?? undefined,
+    );
+  }
+
+  @SlashGroup("방생성", "서버설정")
+  @Slash({
+    name: "조회",
+    description: "모든 방 생성자를 확인합니다.",
+  })
+  async viewRoomMakers(interaction: Discord.ChatInputCommandInteraction) {
+    ServerSettingService.main.showRoomMakers(interaction);
+  }
+}
--- src/discord/commands/ServerSettingService.ts
+++ src/discord/commands/ServerSettingService.ts
@@ -1,167 +1,225 @@
-import ServerSettingManager from "@/core/ServerSettingManager";
-import ServerSettingModel from "@/models/ServerSetting";
 import autoDeleteMessage from "@/utils/autoDeleteMessage";
-import { PermissionGuard } from "@discordx/utilities";
-import {
-  Discord,
-  Guard,
-  Slash,
-  SlashChoice,
-  SlashGroup,
-  SlashOption,
-} from "discordx";
-import MasterGuard from "../guards/MasterGuard";
-import {
-  ApplicationCommandOptionType,
-  channelMention,
-  ChannelType,
-  EmbedBuilder,
-} from "discord.js";
+import { channelMention, EmbedBuilder, inlineCode } from "discord.js";
 import ErrorMessageManager from "../messageManagers/ErrorMessageManager";
-import Vars from "@/Vars";
 import PrimitiveInputMessageManager from "../messageManagers/inputs/PrimitiveInputMessageManager";
-import {
-  InputResolvers,
-  TextInputResolver,
-} from "../messageManagers/inputs/InputResolvers";
+import { InputResolvers } from "../messageManagers/inputs/InputResolvers";
 import RoomMakingDataModel from "@/models/RoomMakingDataModel";
 import RoomMaker from "../features/RoomsMaker";
 
-@Discord()
-@SlashGroup({
-  name: "서버설정",
-  description: "이 서버에 대한 여러 설정을 관리합니다.",
-})
-@Guard(PermissionGuard(["Administrator"]), MasterGuard)
-export class ServerSettingService {
-  @SlashGroup("서버설정")
-  @Slash({ name: "초기화", description: "서버 채널 설정을 새로 정합니다." })
-  async setting(interaction: Discord.ChatInputCommandInteraction) {
-    interaction.deferReply({ ephemeral: true });
-    const serverSettings = await ServerSettingModel.findOne({
-      guildId: interaction.guildId,
-      botId: interaction.client.user!.id,
-    });
-    if (!serverSettings) {
-      throw new Error("서버 설정이 존재치 않는건 불가능합니다.");
-    }
-    autoDeleteMessage(interaction.reply("채널 설정을 변경합니다."), 1500);
-    await ServerSettingManager.main.requestSettingInit(interaction.guild!);
-  }
+export default class ServerSettingService {
+  public static readonly main = new this();
 
-  @SlashGroup("서버설정")
-  @Slash({
-    name: "방생성",
-    description: "해당 채널에 대한 파티방 생성 정보를 조회/수정합니다.",
-  })
-  async roomMakingChannelSetting(
-    @SlashChoice("조회", "추가", "수정", "삭제")
-    @SlashOption({
-      name: "유형",
-      description: "이 설정에서 할 작업",
-      required: true,
-      type: ApplicationCommandOptionType.String,
-    })
-    type: "조회" | "추가" | "수정" | "삭제",
-    @SlashOption({
-      name: "채널",
-      description: "방 생성 정보를 수정할 채널",
-      type: ApplicationCommandOptionType.Channel,
-    })
-    channel: Discord.TextChannel | null,
-
-    interaction: Discord.ChatInputCommandInteraction,
+  public async addRoomMaker(
+    interaction: Discord.RepliableInteraction,
+    roomChannel?: Discord.Channel,
+    roomName?: string,
+    roomDescription?: string,
   ) {
     if (!interaction.channel) return;
 
-    switch (type) {
-      case "조회":
-        await interaction.reply({
-          embeds: [
-            new EmbedBuilder().setTitle("RoomMaker 목록").setDescription(`
+    if (roomChannel) {
+      const exist = await RoomMakingDataModel.findOne({
+        channelId: roomChannel!.id,
+      });
+      if (exist) {
+        autoDeleteMessage(
+          ErrorMessageManager.createOnChannel(interaction.channel, {
+            description: `이미 ${exist.name}(으)로 사용 중인 채널입니다.`,
+          }).then((m) => m.message),
+        );
+        return;
+      }
+    }
+
+    const message = await interaction.reply("방 생성자를 등록합니다.");
+    const render = () =>
+      message.edit(`
+* 채널: ${roomChannel?.id ? channelMention(roomChannel?.id) : "없음"}
+* 이름: ${inlineCode(roomName ?? "없음")}
+* 설명: ${inlineCode(roomDescription ?? "없음")}`);
+    await render();
+
+    if (!roomChannel) {
+      roomChannel = await PrimitiveInputMessageManager.createOnChannel(
+        interaction.channel,
+        {
+          inputResolver: InputResolvers.channel,
+          valueValidators: [
+            {
+              callback: (channel) =>
+                !RoomMaker.main.data.find(
+                  (d) => d.channel.id == (channel as Discord.Channel).id,
+                ),
+              invalidMessage: `${RoomMaker.main.data.map((d) => channelMention(d.channel.id)).join(", ")}들은 이미 사용 중인 채널입니다.`,
+            },
+          ],
+        },
+      ).then((m) => m.value);
+      await render();
+    }
+
+    if (!roomName) {
+      roomName = await PrimitiveInputMessageManager.createOnChannel(
+        interaction.channel,
+        {
+          inputResolver: InputResolvers.text,
+        },
+      ).then((m) => m.value);
+      await render();
+    }
+
+    if (!roomDescription) {
+      roomDescription = await PrimitiveInputMessageManager.createOnChannel(
+        interaction.channel,
+        {
+          inputResolver: InputResolvers.text,
+        },
+      ).then((m) => m.value);
+      await render();
+    }
+
+    await RoomMakingDataModel.create({
+      channelId: roomChannel!.id,
+      name: roomName,
+      description: roomDescription,
+    });
+
+    message.delete();
+    autoDeleteMessage(
+      interaction.channel.send("방 생성자가 등록되었습니다."),
+      1500,
+    );
+  }
+
+  public async editRoomMaker(
+    interaction: Discord.RepliableInteraction,
+    roomChannel?: Discord.Channel,
+    roomName?: string,
+    roomDescription?: string,
+  ) {
+    if (!interaction.channel) return;
+
+    if (roomChannel) {
+      const exist = await RoomMakingDataModel.findOne({
+        channelId: roomChannel!.id,
+      });
+      if (!exist) {
+        autoDeleteMessage(
+          ErrorMessageManager.createOnInteraction(interaction, {
+            description: "해당 채널은 등록되지 않았습니다.",
+          }).then((m) => m.message),
+        );
+        return;
+      }
+    }
+
+    const message = await interaction.reply("방 생성자를 수정합니다.");
+    const render = () =>
+      message.edit(`
+* 채널: ${roomChannel?.id ? channelMention(roomChannel?.id) : "없음"}
+* 이름: ${inlineCode(roomName ?? "없음")}
+* 설명: ${inlineCode(roomDescription ?? "없음")}`);
+    await render();
+
+    if (!roomChannel) {
+      roomChannel = await PrimitiveInputMessageManager.createOnChannel(
+        interaction.channel,
+        {
+          inputResolver: InputResolvers.channel,
+          valueValidators: [
+            {
+              callback: (channel) =>
+                !!RoomMaker.main.data.find(
+                  (d) => d.channel.id == (channel as Discord.Channel).id,
+                ),
+              invalidMessage: `이 채널은 등록되지 않았습니다.
+${RoomMaker.main.data.map((d) => channelMention(d.channel.id)).join(", ")} 사이에서 선택해 주세요.`,
+            },
+          ],
+        },
+      ).then((m) => m.value);
+      await render();
+    }
+
+    if (!roomName) {
+      roomName = await PrimitiveInputMessageManager.createOnChannel(
+        interaction.channel,
+        {
+          inputResolver: InputResolvers.text,
+        },
+      ).then((m) => m.value);
+      await render();
+    }
+
+    if (!roomDescription) {
+      roomDescription = await PrimitiveInputMessageManager.createOnChannel(
+        interaction.channel,
+        {
+          inputResolver: InputResolvers.text,
+        },
+      ).then((m) => m.value);
+      await render();
+    }
+    await RoomMakingDataModel.updateOne(
+      { channelId: roomChannel!.id },
+      {
+        channelId: roomChannel!.id,
+        name: roomName,
+        description: roomDescription,
+      },
+      { upsert: true },
+    );
+
+    message.delete();
+    autoDeleteMessage(
+      interaction.channel.send("방 생성자가 수정되었습니다."),
+      1500,
+    );
+  }
+
+  public async removeRoomMaker(
+    interaction: Discord.RepliableInteraction,
+    roomChannel?: Discord.Channel,
+  ) {
+    if (!interaction.channel) return;
+
+    if (!roomChannel) {
+      roomChannel = await PrimitiveInputMessageManager.createOnChannel(
+        interaction.channel,
+        {
+          inputResolver: InputResolvers.channel,
+          valueValidators: [
+            {
+              callback: (channel) =>
+                !!RoomMaker.main.data.find(
+                  (d) => d.channel.id == (channel as Discord.Channel).id,
+                ),
+              invalidMessage: `이 채널은 등록되지 않았습니다.
+${RoomMaker.main.data.map((d) => channelMention(d.channel.id)).join(", ")} 사이에서 선택해 주세요.`,
+            },
+          ],
+        },
+      ).then((m) => m.value);
+    }
+
+    await RoomMakingDataModel.deleteOne({ channelId: roomChannel!.id });
+
+    autoDeleteMessage(
+      interaction.reply("성공적으로 방 생성자를 삭제했습니다."),
+      1500,
+    );
+  }
+
+  public async showRoomMakers(interaction: Discord.RepliableInteraction) {
+    if (!interaction.channel) return;
+
+    await interaction.reply({
+      embeds: [
+        new EmbedBuilder().setTitle("RoomMaker 목록").setDescription(`
       ${RoomMaker.main.data
         .map((data) => `* ${data.name}: ${channelMention(data.channel.id)}`)
         .join("\n")}`),
-          ],
-        });
-        break;
-      case "추가":
-      // TODO: implement add room making
-      case "수정":
-      case "삭제":
-        if (!channel) {
-          const nameAskMsg = await interaction.channel
-            .send(`수정/삭제할 채널이 필요합니다. 
- 채널을 입력해주세요.`);
-          channel = await PrimitiveInputMessageManager.createOnChannel(
-            interaction.channel,
-            {
-              inputResolver: InputResolvers.channel,
-              valueValidators: [
-                {
-                  callback: (channel) =>
-                    !!RoomMaker.main.data.find(
-                      (d) => d.channel.id == (channel as Discord.Channel).id,
-                    ),
-                  invalidMessage: `유효하지 않은 값입니다.
-  가능한 값: ${RoomMaker.main.data.map((d) => d.name).join(", ")}`,
-                },
-              ],
-            },
-          ).then((m) => m.value as Discord.TextChannel);
-          nameAskMsg.delete();
-        } else if (channel.type !== ChannelType.GuildText) {
-          autoDeleteMessage(
-            ErrorMessageManager.createOnInteraction(interaction, {
-              description: "텍스트 채널이어야 합니다.",
-            }).then((m) => m.message),
-          );
-          return;
-        }
-        if (!channel) {
-          RoomMaker.main.sendErrorMessage(
-            interaction,
-            "채널을 찾을 수 없습니다.",
-          );
-          return;
-        }
-
-        if (type == "삭제") {
-          RoomMaker.main.data.splice(
-            RoomMaker.main.data.findIndex((d) => d.channel.id == channel!.id),
-          );
-          await RoomMakingDataModel.deleteOne({ channelId: channel.id });
-        } else {
-          const descriptionAskMsg =
-            await interaction.channel.send("설명을 입력해주세요.");
-          const description =
-            await PrimitiveInputMessageManager.createOnChannel(
-              interaction.channel,
-              {
-                inputResolver: new TextInputResolver(),
-                value: Vars.roomMakingAnnounceData[channel.id]?.description,
-              },
-            ).then((m) => m.value);
-
-          await Promise.all([
-            descriptionAskMsg.delete(),
-            RoomMakingDataModel.updateOne(
-              { channelId: channel.id },
-              {
-                channelId: channel.id,
-                name: channel.name,
-                description,
-              },
-              { upsert: true },
-            ),
-          ]);
-        }
-        break;
-    }
-
-    autoDeleteMessage(
-      interaction.channel.send("방 정보가 수정되었습니다."),
-      1500,
-    );
+      ],
+    });
   }
 }
