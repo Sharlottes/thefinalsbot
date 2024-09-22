bd1f051b92da0feace595fc88188db548ccebb97
sharlotte <aaa9810321@gmail.com>
2024-09-23 02:17:48 +0900

    fix: #36 button component 및 키패드 삭제

--- src/discord/messageManagers/PaginationMessageManager.ts
+++ src/discord/messageManagers/PaginationMessageManager.ts
@@ -1,12 +1,12 @@
 import { PrimitiveInputType, TextInputResolver } from "@/discord/messageManagers/inputs/InputResolvers";
 import PrimitiveInputMessageManager from "@/discord/messageManagers/inputs/PrimitiveInputMessageManager";
-import ButtonComponent from "@/discord/components/ButtonComponent";
 import MessageManager, { MessageData } from "@/discord/messageManagers/MessageManager";
-import { ActionRowBuilder, ButtonStyle } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 import EventEmitter from "node:events";
 import TypedEmitter from "typed-emitter";
 import Vars from "@/Vars";
 import throwInteraction from "@/utils/throwInteraction";
+import { ButtonComponent } from "discordx";
 
 const btn_symbols = [
   { symbol: "⏪", page: -10 },
@@ -58,11 +58,19 @@ export default class PaginationMessageManager extends MessageManager<PaginationO
       if (msg.id === message.id) {
         manager.events.emit("end");
         Vars.client.off("messageDelete", deleteHandler);
+        Vars.client.off("interactionCreate", manager.handleInteraction);
       }
     };
+    Vars.client.on("interactionCreate", manager.handleInteraction);
     Vars.client.on("messageDelete", deleteHandler);
 
     return manager;
+  }
+
+  handleInteraction(interaction: Discord.Interaction) {
+    if (!interaction.isButton()) return;
+    if (interaction.customId == "pagination-select-page") this.paginationSelectPage(interaction);
+    else if (interaction.customId.startsWith("pagination-move-")) this.paginationMove(interaction);
   }
 
   private async updateChanges() {
@@ -80,57 +88,59 @@ export default class PaginationMessageManager extends MessageManager<PaginationO
     this.events.emit("change");
   }
 
+  private readonly onlyUint = {
+    callback: (value: string) => value.match(/^\d+$/) !== null,
+    invalidMessage: "자연수만 가능합니다.",
+  };
+  private readonly onlyInRange = {
+    callback: (value: PrimitiveInputType) => +value >= 1 && +value <= this.size,
+    invalidMessage: `1부터 ${this.size} 사이의 숫자만 가능합니다.`,
+  };
+  private async paginationSelectPage(interaction: Discord.ButtonInteraction) {
+    PrimitiveInputMessageManager.createOnInteraction(interaction, {
+      inputResolver: new TextInputResolver(),
+      textValidators: [this.onlyUint],
+      valueValidators: [this.onlyInRange],
+      onConfirm: (amount) => {
+        this.currentPage = +amount - 1;
+        this.updateChanges();
+      },
+    });
+  }
+
+  private async paginationMove(interaction: Discord.ButtonInteraction) {
+    throwInteraction(interaction);
+    const index = +interaction.customId.split("-")[2];
+    this.currentPage += btn_symbols[index].page;
+    this.updateChanges();
+  }
+
   private buildButtons() {
-    const action_rows = new ActionRowBuilder<ButtonComponent>();
+    const action_rows = new ActionRowBuilder<ButtonBuilder>();
 
     for (let i = 0; i < btn_symbols.length; i++) {
       if (btn_symbols[i].symbol == "/") {
         action_rows.addComponents(
-          ButtonComponent.create(
-            {
-              label: `${this.currentPage + 1} / ${this.size}`,
-              disabled: this.size === 1,
-              style: ButtonStyle.Secondary,
-            },
-            (interaction) => {
-              const onlyUint = {
-                callback: (value: string) => value.match(/^\d+$/) !== null,
-                invalidMessage: "자연수만 가능합니다.",
-              };
-              const onlyInRange = {
-                callback: (value: PrimitiveInputType) => +value >= 1 && +value <= this.size,
-                invalidMessage: `1부터 ${this.size} 사이의 숫자만 가능합니다.`,
-              };
-              PrimitiveInputMessageManager.createOnInteraction(interaction, {
-                inputResolver: new TextInputResolver(),
-                textValidators: [onlyUint],
-                valueValidators: [onlyInRange],
-                onConfirm: (amount) => {
-                  this.currentPage = +amount - 1;
-                  this.updateChanges();
-                },
-              });
-            },
-          ),
+          new ButtonBuilder()
+            .setLabel(`${this.currentPage + 1} / ${this.size}`)
+            .setDisabled(this.size === 1)
+            .setStyle(ButtonStyle.Secondary)
+            .setCustomId("pagination-select-page"),
         );
         continue;
       }
 
+      const isDisabled =
+        this.currentPage + btn_symbols[i].page < 0 || this.currentPage + btn_symbols[i].page > this.size - 1;
       action_rows.addComponents(
-        ButtonComponent.create(
-          {
-            emoji: btn_symbols[i].symbol,
-            disabled:
-              this.currentPage + btn_symbols[i].page < 0 || this.currentPage + btn_symbols[i].page > this.size - 1,
-          },
-          (inter) => {
-            throwInteraction(inter);
-            this.currentPage += btn_symbols[i].page;
-            this.updateChanges();
-          },
-        ),
+        new ButtonBuilder()
+          .setEmoji(btn_symbols[i].symbol)
+          .setDisabled(isDisabled)
+          .setStyle(ButtonStyle.Primary)
+          .setCustomId("pagination-move-" + i),
       );
     }
+
     return action_rows;
   }
 }
