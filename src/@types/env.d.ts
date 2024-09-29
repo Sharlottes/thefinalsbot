290a10aefb9cc21c285e5c7e2dd195b4b5d71b6a
sharlotte <aaa9810321@gmail.com>
2024-09-29 20:44:08 +0900

    refactor: update env type and example

--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -3,5 +3,7 @@ interface Env {
   TEST_GUILD_ID: string;
   MASTER_USERS: string;
   MONGO_URL: string;
+  DB_NAME: string;
+  // * NODE_ENV는 package.json의 scripts에 의해 정해집니다
   NODE_ENV: "development" | "production";
 }
